0f0d989200ced86db4a2f2b02ed76c6bc42004fe
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-04-06 20:13:47 +0700

    initional

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+var mySwiper = (window.innerWidth > 800) ? new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  speed: 1000,
+  mousewheel: true,
+  keyboard: true,
+  sensitivity: -100,
+}) : 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  mySwiper.slideNext()
+})
+
+if (window.innerWidth <= 800) {
+  const sections = document.querySelectorAll('.section')
+  // Удаление слайдера 
+  document.querySelector('.swiper-container').remove()
+  // Возвращение секций обратно 
+  for (let i = sections.length - 1; i >= 0; i--) {
+    document.querySelector('.header').after(sections[i])
+  }
+
+}
