18a1db689027a6cdcb0694416d17244b0402b43a
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-04-08 03:29:57 +0700

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,5 @@
 if (window.innerWidth > 800) {
   var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     speed: 1000,
@@ -8,36 +7,30 @@ if (window.innerWidth > 800) {
     keyboard: true,
     sensitivity: -100,
   });
-
+// Логотип в header и его анимация
   const logo = document.querySelector('.header-logo')
   const post = document.querySelector('.header-item:nth-child(2)')
 
   mySwiper.on('slideChange', () => {
     if (mySwiper.activeIndex == 0) {
       logo.style.transform = 'translateY(-100px)'
-      // logo.style.display = 'none'
-      // post.classList.remove('post')
       setTimeout(() => {
         post.style.marginLeft = '0'
-
       }, 100);
-
     } else {
-      // logo.style.display = 'block'
-
       logo.style.transform = 'translateY(0)'
       post.style.marginLeft = 'auto'
-      // post.classList.add('post')
-
     }
-    console.log(post)
   })
+
+  document.querySelector('.header-logo').addEventListener('click', () => {
+    mySwiper.slideTo(0)
+  })
+// Навигация
   document.querySelectorAll('.next').forEach(item => {
     item.addEventListener('click', () => {
       mySwiper.slideNext()
-
     })
-
   });
 
   document.querySelectorAll('.prev').forEach(item => {
@@ -46,9 +39,6 @@ if (window.innerWidth > 800) {
     })
   });
 
-  document.querySelector('.header-logo').addEventListener('click', () => {
-    mySwiper.slideTo(0)
-  })
 } else {
   const sections = document.querySelectorAll('.section')
   // Удаление слайдера 
