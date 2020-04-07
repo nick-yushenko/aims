25ceb1ba9f6c0e05b01d2ff2f7c237720d0ba2f3
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-04-08 00:31:41 +0700

    finish2

--- js/slider.js
+++ js/slider.js
@@ -1,53 +1,55 @@
-var mySwiper = (window.innerWidth > 800) ? new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
-  speed: 1000,
-  mousewheel: true,
-  keyboard: true,
-  sensitivity: -100,
-}) : 0;
+if (window.innerWidth > 800) {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    speed: 1000,
+    mousewheel: true,
+    keyboard: true,
+    sensitivity: -100,
+  });
 
-const logo = document.querySelector('.header-logo')
-const post = document.querySelector('.header-item:nth-child(2)')
+  const logo = document.querySelector('.header-logo')
+  const post = document.querySelector('.header-item:nth-child(2)')
 
-mySwiper.on('slideChange', () => {
-  if (mySwiper.activeIndex == 0) {
-    logo.style.transform = 'translateY(-100px)'
-    // logo.style.display = 'none'
-    post.classList.remove('post')
-    post.style.marginLeft = '0'
+  mySwiper.on('slideChange', () => {
+    if (mySwiper.activeIndex == 0) {
+      logo.style.transform = 'translateY(-100px)'
+      // logo.style.display = 'none'
+      // post.classList.remove('post')
+      setTimeout(() => {
+        post.style.marginLeft = '0'
 
-  } else {
-    // logo.style.display = 'block'
+      }, 100);
 
-    logo.style.transform = 'translateY(0)'
-    post.style.marginLeft = '70px'
-    post.classList.add('post')
+    } else {
+      // logo.style.display = 'block'
 
-  }
-  console.log(post)
-})
-document.querySelectorAll('.next').forEach(item => {
-  item.addEventListener('click', () => {
-    mySwiper.slideNext()
+      logo.style.transform = 'translateY(0)'
+      post.style.marginLeft = 'auto'
+      // post.classList.add('post')
 
+    }
+    console.log(post)
   })
+  document.querySelectorAll('.next').forEach(item => {
+    item.addEventListener('click', () => {
+      mySwiper.slideNext()
 
-});
+    })
 
-document.querySelectorAll('.prev').forEach(item => {
-  item.addEventListener('click', () => {
-    mySwiper.slidePrev()
+  });
+
+  document.querySelectorAll('.prev').forEach(item => {
+    item.addEventListener('click', () => {
+      mySwiper.slidePrev()
+    })
+  });
+
+  document.querySelector('.header-logo').addEventListener('click', () => {
+    mySwiper.slideTo(0)
   })
-});
-
-document.querySelector('.header-logo').addEventListener('click', () => {
-  mySwiper.slideTo(0)
-})
-
-
-if (window.innerWidth <= 800) {
+} else {
   const sections = document.querySelectorAll('.section')
   // Удаление слайдера 
   document.querySelector('.swiper-container').remove()
