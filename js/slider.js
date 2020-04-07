53555453df22e9000371d7b69ec33f9593610496
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-04-07 23:36:23 +0700

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -8,10 +8,32 @@ var mySwiper = (window.innerWidth > 800) ? new Swiper('.swiper-container', {
   sensitivity: -100,
 }) : 0;
 
+const logo = document.querySelector('.header-logo')
+const post = document.querySelector('.header-item:nth-child(2)')
+
+mySwiper.on('slideChange', () => {
+  if (mySwiper.activeIndex == 0) {
+    logo.style.transform = 'translateY(-100px)'
+    // logo.style.display = 'none'
+    post.classList.remove('post')
+    post.style.marginLeft = '0'
+
+  } else {
+    // logo.style.display = 'block'
+
+    logo.style.transform = 'translateY(0)'
+    post.style.marginLeft = '70px'
+    post.classList.add('post')
+
+  }
+  console.log(post)
+})
 document.querySelectorAll('.next').forEach(item => {
   item.addEventListener('click', () => {
     mySwiper.slideNext()
+
   })
+
 });
 
 document.querySelectorAll('.prev').forEach(item => {
@@ -19,6 +41,10 @@ document.querySelectorAll('.prev').forEach(item => {
     mySwiper.slidePrev()
   })
 });
+
+document.querySelector('.header-logo').addEventListener('click', () => {
+  mySwiper.slideTo(0)
+})
 
 
 if (window.innerWidth <= 800) {
