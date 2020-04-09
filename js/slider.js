e674260ac129298b85dc9e5a644f74cd7c9162b3
nick-yushenko <59775649+nick-yushenko@users.noreply.github.com>
2020-04-10 04:22:47 +0700

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -2,16 +2,21 @@ if (window.innerWidth > 800) {
   var mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: false,
-    speed: 1000,
-    mousewheel: true,
+    speed: 700,
+    mousewheelControl: true,
     keyboard: true,
-    sensitivity: -100,
+    // sensitivity: 3,
+    // releaseOnEdges: true,
+    // sensitivity: 0.01,
+    // freeModeMomentum: false,
+    // freeMode: true
   });
-// Логотип в header и его анимация
+  // Логотип в header и его анимация
   const logo = document.querySelector('.header-logo')
   const post = document.querySelector('.header-item:nth-child(2)')
 
-  mySwiper.on('slideChange', () => {
+  mySwiper.on('change', () => {
+    console.log('1')
     if (mySwiper.activeIndex == 0) {
       logo.style.transform = 'translateY(-100px)'
       setTimeout(() => {
@@ -26,7 +31,7 @@ if (window.innerWidth > 800) {
   document.querySelector('.header-logo').addEventListener('click', () => {
     mySwiper.slideTo(0)
   })
-// Навигация
+  // Навигация
   document.querySelectorAll('.next').forEach(item => {
     item.addEventListener('click', () => {
       mySwiper.slideNext()
@@ -49,3 +54,63 @@ if (window.innerWidth > 800) {
   }
 
 }
+
+// скролл слайдера 
+
+
+// function debounce(f,  ms) {
+
+//   let isCooldown = false;
+
+//   return function () {
+//     if (isCooldown) return;
+
+//     f.apply(this, arguments);
+
+//     isCooldown = true;
+
+//     setTimeout(() => isCooldown = false, ms);
+//   };
+
+// }
+
+// let next = debounce(mySwiper.slideNext, 1000)
+// let deltaH = 0
+// let timerId = setInterval(function () {
+//   if (deltaH >= 100) {
+//     deltaH = 0
+
+//     next(100)
+//   }
+//   if (deltaH <= -100) {
+//     deltaH = 0
+//     mySwiper.slidePrev()
+//   }
+
+// }, 500);
+// const elem = document.querySelector('html body')
+// if (elem.addEventListener) {
+//   if ('onwheel' in document) {
+//     // IE9+, FF17+
+//     elem.addEventListener("wheel", onWheel, false);
+//   } else if ('onmousewheel' in document) {
+//     // устаревший вариант события
+//     elem.addEventListener("mousewheel", onWheel, false);
+//   } else {
+//     // 3.5 <= Firefox < 17, более старое событие DOMMouseScroll пропустим
+//     elem.addEventListener("MozMousePixelScroll", onWheel, false);
+//   }
+// } else { // IE<9
+//   elem.attachEvent("onmousewheel", onWheel);
+// }
+
+// function onWheel(e) {
+//   e = e || window.event;
+
+//   // wheelDelta не дает возможность узнать количество пикселей
+//   var delta = e.deltaY || e.detail || e.wheelDelta;
+
+//   deltaH += delta
+
+//   // e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+// }
